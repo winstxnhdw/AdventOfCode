464a6b0d5ed544f08599673e192c3ec84bfdc3cc
winstxnhdw <winstxnhdw@gmail.com>
2022-12-09 17:41:28 +0800

    day 5 finally

---
+++ 2022/5/I/src/main.rs
@@ -0,0 +1,102 @@
+use std::fs;
+
+struct Schematic {
+    drawing: String,
+    procedure: String,
+}
+
+fn get_schematic() -> Schematic {
+    let schematic_content = fs::read_to_string("../schematic.txt")
+        .unwrap()
+        .split("\n\n")
+        .map(str::to_string)
+        .collect::<Vec<String>>();
+
+    Schematic {
+        drawing: schematic_content[0].clone(),
+        procedure: schematic_content[1].clone(),
+    }
+}
+
+fn transpose<T>(vector: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let len = vector[0].len();
+    let mut iters: Vec<_> = vector.into_iter().map(|n| n.into_iter()).collect();
+
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
+}
+
+fn get_drawing_vector(drawing: &str) -> Vec<Vec<char>> {
+    let drawing_lines = drawing
+        .lines()
+        .map(|line| line.chars().collect())
+        .rev()
+        .collect::<Vec<Vec<char>>>();
+
+    transpose(drawing_lines)
+        .into_iter()
+        .filter_map(|c| match c.first().unwrap().is_digit(10) {
+            true => Some(
+                c.into_iter()
+                    .skip(1)
+                    .filter(|c| c.is_alphabetic())
+                    .collect(),
+            ),
+            _ => None,
+        })
+        .collect()
+}
+
+fn get_procedure_vector(procedure: &str) -> Vec<Vec<usize>> {
+    procedure
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|word| match word.parse::<usize>() {
+                    Ok(stack_number) => Some(stack_number),
+                    _ => None,
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn rearrange(
+    drawing_vector: &Vec<Vec<char>>,
+    procedure_vector: &Vec<Vec<usize>>,
+) -> Vec<Vec<char>> {
+    let mut rearranged_stack: Vec<Vec<char>> = drawing_vector.to_vec();
+
+    for procedure in procedure_vector {
+        let chosen_stack = procedure[1] - 1;
+        let popped_length = rearranged_stack[chosen_stack].len() - procedure[0];
+        let popped_crates = rearranged_stack[chosen_stack]
+            .drain(popped_length..)
+            .rev()
+            .collect::<Vec<char>>();
+        rearranged_stack[procedure[2] - 1].extend(popped_crates);
+    }
+
+    rearranged_stack
+}
+
+fn main() {
+    let scheme = get_schematic();
+    let drawing_vector = get_drawing_vector(&scheme.drawing);
+    let procedure_vector = get_procedure_vector(&scheme.procedure);
+    let rearranged_stack = rearrange(&drawing_vector, &procedure_vector);
+
+    println!(
+        "{:?}",
+        rearranged_stack
+            .iter()
+            .map(|stack| stack.last().unwrap())
+            .collect::<String>()
+    );
+}
---
+++ 2022/5/README.md
@@ -0,0 +1,21 @@
+# Day 5: Supply Stacks
+
+The expedition can depart as soon as the final supplies have been unloaded from the ships. Supplies are stored in stacks of marked crates, but because the needed supplies are buried under many other crates, the crates need to be rearranged.
+
+The ship has a giant cargo crane capable of moving crates between stacks. To ensure none of the crates get crushed or fall over, the crane operator will rearrange them in a series of carefully-planned steps. After the crates are rearranged, the desired crates will be at the top of each stack.
+
+The Elves don't want to interrupt the crane operator during this delicate procedure, but they forgot to ask her which crate will end up where, and they want to be ready to unload them as soon as possible so they can embark.
+
+They do, however, have a drawing of the starting stacks of crates and the rearrangement procedure (your puzzle input). For example:
+
+```txt
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+```
