90f25cd3b7c35b495bfd34e0973f6e50dc12b6fd
winstxnhdw <winstxnhdw@gmail.com>
2022-12-12 22:46:05 +0800

    day 6

---
+++ 2022/6/I/src/main.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn get_next_start_index(substring: &str) -> i32 {
+    let mut character_map = HashMap::new();
+
+    for (i, c) in substring.chars().enumerate() {
+        if character_map.contains_key(&c) {
+            return 1 + *character_map.get(&c).unwrap() as i32;
+        }
+
+        character_map.insert(c, i);
+    }
+
+    return -1;
+}
+
+fn get_start_of_packet_marker() -> i32 {
+    let datastream_buffer = fs::read_to_string("../datastream_buffer.txt").unwrap();
+    let slice_length = 4;
+    let mut start_slice: i32 = 0;
+
+    loop {
+        let end_slice: i32 = start_slice + slice_length;
+        let start_of_packet_marker = &datastream_buffer[start_slice as usize..end_slice as usize];
+        let next_start_index = get_next_start_index(start_of_packet_marker);
+
+        if next_start_index == -1 {
+            return end_slice;
+        }
+
+        start_slice += next_start_index;
+    }
+}
+
+fn main() {
+    println!(
+        "Characters before the first start-of-packet marker: {}",
+        get_start_of_packet_marker()
+    );
+}
---
+++ 2022/6/README.md
@@ -0,0 +1,11 @@
+# Day 6: Tuning Trouble
+
+The preparations are finally complete; you and the Elves leave camp on foot and begin to make your way toward the star fruit grove.
+
+As you move through the dense undergrowth, one of the Elves gives you a handheld device. He says that it has many fancy features, but the most important one to set up right now is the communication system.
+
+However, because he's heard you have significant experience dealing with signal-based systems, he convinced the other Elves that it would be okay to give you their one malfunctioning device - surely you'll have no problem fixing it.
+
+As if inspired by comedic timing, the device emits a few colorful sparks.
+
+To be able to communicate with the Elves, the device needs to lock on to their signal. The signal is a series of seemingly-random characters that the device receives one at a time.
